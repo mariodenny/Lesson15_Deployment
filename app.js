6cf18280609c45391a907557663bb0c041c07d25
mariodeny <dennym.channel@gmail.com>
2026-06-26 11:38:56 +0700

    render index view

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import './db.js'
+import { engine } from 'express-handlebars' 
 
 const app = express()
 const port = process.env.PORT
 console.log(`Port for express ${port}`)
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
 app.get("/", (req,res)=>{
-    res.send(`Env loaded successfully! Example : ${port}`)
+    res.render("index", {
+        title : "Home",
+        message : "I built this apps using expres and handlebars, mongodb as my database.",
+        author : "mariodenny on github"
+    })
 })
 
 app.listen(port, ()=>{
